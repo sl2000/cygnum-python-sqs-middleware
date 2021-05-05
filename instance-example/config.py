4c4133ca3f004cba75ba93233f2909fa87b9f614
Simon Lewington <sl2000@gmail.com>
2021-05-05 10:07:18 +0000

    move config files to instance

--- instance-example/config.py
+++ instance-example/config.py
@@ -1,12 +1,1 @@
-import logging
-
-ACCOUNTS = ["ob_live", "ob_train"]
-UNIDATA_SERVER_ID = "ken-esdev-01"
-TIMEOUT = 180 # Max time in seconds to wait for server response - this is between 'pings' - the max total time is in nginx.conf and gunicorn.conf.py
-MAX_CONTENT_LENGTH = 16 * 1024 # Max upload size
-LOGLEVEL = logging.WARNING
-# AWS Settings
-BUCKET_NAME = "cygnum.XXX.es.caci.co.uk"
-AWS_ACCESS_KEY = ""
-AWS_SECRET_KEY = ""
-AWS_REGION = "eu-west-2"
+# blank file needed - i don't know why
