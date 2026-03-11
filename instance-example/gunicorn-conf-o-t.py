27b7eb11bf5482fc8c47bc6faddb27798c212bf0
root <root@ip-172-31-19-7.eu-west-2.compute.internal>
2026-03-11 11:56:53 +0000

    gunicorn example config update - gevent, control socket

--- instance-example/gunicorn-conf-o-t.py
+++ instance-example/gunicorn-conf-o-t.py
@@ -1,13 +1,13 @@
 import multiprocessing
 
-bind = "unix:instance/cygnum-o-t.sock"
+bind = "unix:/home/ubuntu/cygnum/instance/cygnum-o-t.sock"
 umask = 0o007
 
 backlog = 2048
 
 #workers = multiprocessing.cpu_count() * 2 + 1
 workers = 1
-worker_class = 'eventlet'
+worker_class = 'gevent'
 worker_connections = 100
 
 max_requests = 1000 # This is a simple method to help limit the damage of memory leaks.
@@ -15,5 +15,8 @@ max_requests_jitter = 50
 
 graceful_timeout = 120 # After receiving a restart signal, workers have this much time to finish serving requests. Workers still alive after the timeout (starting from the receipt of the restart signal) are force killed.
 
-timeout = 30 # Workers silent for more than this many seconds are killed and restarted.
+timeout = 1800 # Workers silent for more than this many seconds are killed and restarted.
 keepalive = 2
+
+control_socket_disable = True
+
